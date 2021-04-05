02023f367c5a49ca0ccdcf9ed19ccd590d64b870
ArekStasko <arek.stasko@gmail.com>
2021-04-05 19:05:14 +0200

    add full projects part of main site without media queries and scroll animation

--- src/components/projectsPart/ProjectsPart.js
+++ src/components/projectsPart/ProjectsPart.js
@@ -7,10 +7,22 @@ import './ProjectsPart.css'
 gsap.registerPlugin(ScrollTrigger)
 
 
+const MouseMove = e => {
+    const positionX = e.clientX / window.innerWidth - 0.55
+    const positionY = e.clientY / window.innerHeight - 0.55
+
+    gsap.to(".project-photo", {
+      rotationY: positionX * 50,
+      rotationX: -positionY * 50,
+      ease: "none",
+    })
+  }
+
+
 const ProjectsPart = () => {
 
     return(
-        <div className='main_projects-container'>
+        <div className='main_projects-container' onMouseMove={e => MouseMove(e)}>
 
             <div className='Projects-header'>
             <h1>Hello, i'm Arek and i would like to introduce you myself</h1>
@@ -21,9 +33,9 @@ const ProjectsPart = () => {
                 <div className='Projects_photos'>
  <StaticImage
             layout="constrained"
-            width={400}
+            width={500}
             placeholder="tracedSVG"
-            className='portfolio_template'
+            className='project-photo portfolio_template'
             quality="100"
             src="../../assets/images/portfolio_template.png"
             alt="project-photo"
@@ -31,7 +43,7 @@ const ProjectsPart = () => {
   <StaticImage
             layout="constrained"
             width={500}
-            className='whats_matter'
+            className='project-photo whats_matter'
             placeholder="tracedSVG"
             quality="100"
             src="../../assets/images/whats_matter.png"
@@ -39,8 +51,8 @@ const ProjectsPart = () => {
           />
   <StaticImage
             layout="constrained"
-            width={300}
-            className='task_and_I'
+            width={450}
+            className='project-photo task_and_I'
             placeholder="tracedSVG"
             quality="100"
             src="../../assets/images/task_and_I.png"
@@ -49,7 +61,10 @@ const ProjectsPart = () => {
                 </div>
 
                 <div className='Projects_description'>
-<h2>Joł opis</h2>
+<h2>I try to do as many projects as possible, trying to keep rules such as dry or kiss</h2>
+<div>
+ <a href='/'>See all projects &#10; &#x02192;</a>
+</div>
                 </div>
             </div>
         </div>
